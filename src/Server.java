86a6a0dc7ca59c7a69dbb88209ed2c85835b0819
ashwinka <ashwinka@INashwinka.local>
2016-03-09 11:06:00 +0530

    Created Server side TCP

---
+++ src/Server.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws Exception{
+        String clientText;
+        String textToUpperCase;
+        ServerSocket recieveClientSocket = new ServerSocket(1234);
+
+        while(true){
+            Socket connectionSocket = recieveClientSocket.accept();
+
+            BufferedReader recieveInputFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            DataOutputStream provideOutputToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientText = recieveInputFromClient.readLine();
+
+            textToUpperCase = clientText.toUpperCase();
+
+            provideOutputToClient.writeBytes(textToUpperCase);
+        }
+    }
+}
